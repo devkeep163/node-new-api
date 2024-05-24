a0636a35d3a1e6f2016b123e3226212204b423bf
100skeep <100@skeep.cc>
2024-05-24 23:06:38 +0800

    auto

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.json());
-app.use(express.text());
+app.use(express.urlencoded({ extended: true }));
 
 function isValidJson(str) {
     try {
